d6be989fa7e728fea17e453ff0b5808aeea2684b
unknown <patmack16@gmail.com>
2020-07-16 13:29:26 -0500

    and after saving

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var app = express();
 app.use(helmet());
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://test:test@cluster0-ksmgo.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://test:test@cluster0-ksmgo.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
